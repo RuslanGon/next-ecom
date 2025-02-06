6c1003f4a9f433fe1190878b29dbe85ceccf63b7
RuslanGon <ruslan441006@gmail.com>
2025-02-06 11:43:55 +0200

    wer

--- components/Product.tsx
+++ components/Product.tsx
@@ -9,7 +9,9 @@ const Product = ({ product }: Props) => {
     <Link
       href={`/product/${product.id}`}
       className="h-96 flex flex-col border group-hover:scale-105 transition-transform ease-out duration-200">
-      <div></div>
+      <div>
+        
+      </div>
       <div className="font-semibold flex items-center justify-between mt-4 mb-1">
         <p>{product.title}</p>
         <p>${product.price}</p>
