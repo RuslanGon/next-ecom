9e8491df238b80713954664f6108822ae4b3e7e8
RuslanGon <ruslan441006@gmail.com>
2025-02-07 14:42:29 +0200

    wer

---
+++ app/(.)product/[id]/page.tsx
@@ -0,0 +1,17 @@
+'use client'
+import ProductImage from "@/components/ProductImage";
+import { Dialog } from "@headlessui/react";
+import { StarIcon as StarIconOutline } from "@heroicons/react/24/outline";
+import { StarIcon } from "@heroicons/react/20/solid";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
