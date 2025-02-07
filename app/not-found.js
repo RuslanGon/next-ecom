e68c7506664e3c92b24cffa98c0dc20d3fc9cce8
RuslanGon <ruslan441006@gmail.com>
2025-02-07 09:28:46 +0200

    wer

---
+++ app/not-found.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
+      <h2 className="text-6xl font-bold text-red-500">Страница не найдена</h2>
+      <p className="text-lg text-gray-600 mt-2">Такой страницы не существует.</p>
+      <Link
+        href="/"
+        className="mt-6 px-6 py-3 bg-blue-600 text-white text-lg rounded-lg shadow-md hover:bg-blue-700 transition duration-300"
+      >
+        Вернуться на главную
+      </Link>
+    </div>
+  );
+}
